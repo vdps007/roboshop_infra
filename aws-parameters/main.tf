3b7565c17a905720a634eb345ea51376b80651fc
vdps <vdps007@gmail.com>
2023-03-10 19:51:09 +0530

    adding makefile

--- aws-parameters/main.tf
+++ aws-parameters/main.tf
@@ -1,15 +1,15 @@
 resource "aws_ssm_parameter" "parameter" {
   count = length(var.parameters)
-  name  = var.parameters[count.index].name
-  type  = var.parameters[count.index].type
-  value = var.parameters[count.index].value
+  name  = "var.parameters[count.index].name"
+  type  = "var.parameters[count.index].type"
+  value = "var.parameters[count.index].value"
 }
 
 resource "aws_ssm_parameter" "secret" {
   count = length(var.secrets)
-  name  = var.secrets[count.index].name
-  type  = var.secrets[count.index].type
-  value = var.secrets[count.index].value
+  name  = "var.secrets[count.index].name"
+  type  = "var.secrets[count.index].type"
+  value = "var.secrets[count.index].value"
 }
 variable "parameters" {}
 variable "secrets" {}
