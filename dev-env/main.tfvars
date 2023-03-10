2bf1ea62a2a0538d2da96b531ece78589c74e816
vdps <vdps007@gmail.com>
2023-03-10 17:32:33 +0530

    adding makefile

--- dev-env/main.tfvars
+++ dev-env/main.tfvars
@@ -4,10 +4,7 @@ instances = {
       type = "t2.micro"
       monitor = true
     }
-    mongodb = {
-      name = "mongodb"
-      type = "t2.micro"
-    }
+    
   }
 
   env = "dev"
--- ec2/main.tf
+++ ec2/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
+  iam_instance_profile = "${var.env}-${var.component}-role"
   tags = {
     Name = var.component
     Monitor = var.monitor ? "yes" : "no"
@@ -93,14 +94,10 @@ resource "aws_iam_role" "role" {
     "Statement": [
         {
             "Effect": "Allow",
-            "Action": [
-                "sts:AssumeRole"
-            ],
             "Principal": {
-                "Service": [
-                    "ec2.amazonaws.com"
-                ]
-            }
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
         }
     ]
 })
