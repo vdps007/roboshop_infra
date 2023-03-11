eb1f72c79662dacc24ec7e3ac75428bbf35b209a
vdps <vdps007@gmail.com>
2023-03-11 15:09:34 +0530

    test update

--- aws-parameters/main.tf
+++ aws-parameters/main.tf
@@ -5,6 +5,13 @@ resource "aws_ssm_parameter" "parameter" {
   value = var.parameters[count.index].value
 }
 
+resource "aws_ssm_parameter" "secret" {
+  count = length(var.secrets)
+  name  = var.secrets[count.index].name
+  type  = var.secrets[count.index].type
+  value = var.secrets[count.index].value
+}
+
 
 variable "parameters" {}
 variable "secrets" {}
